a80bd171edadc959a1467d73f5f762340dbb5f74
jebuAsus <95964955+Carbrex@users.noreply.github.com>
2023-05-28 13:34:32 +0530

    added configuration for render hosting

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 require('express-async-errors');
-
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const authRouter = require('./routes/auth.js');
@@ -16,10 +16,16 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-// routes
+
 app.use('/assets', express.static('./public/uploads'));
 app.use('/api/auth', authRouter);
 app.use('/api/images', authenticateUser, imageRouter);
+app.use(express.static('../frontend/dist'));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../frontend/dist/', 'index.html'));
+});
+
+// routes
 
 const port = process.env.PORT || 8080;
 const start = async () => {
